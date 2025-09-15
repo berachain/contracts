edeb6511c0db8d054e939db5a3a67a218cc8f4b8
barny <32982922+sj448@users.noreply.github.com>
2025-09-15 14:32:56 +0530

    add BGT deployer (#50)

--- script/pol/deployment/2_DeployBGT.s.sol
+++ script/pol/deployment/2_DeployBGT.s.sol
@@ -4,15 +4,16 @@ pragma solidity 0.8.26;
 import { console2 } from "forge-std/console2.sol";
 import { BaseScript } from "../../base/Base.s.sol";
 import { ForceTransferBera } from "../logic/ForceTransferBera.sol";
-import { BGTDeployer } from "../logic/BGTDeployer.sol";
+import { BGTDeployer } from "src/pol/BGTDeployer.sol";
 import { BGT_ADDRESS } from "../POLAddresses.sol";
 import { BGT_SALT } from "../POLSalts.sol";
 
-contract DeployBGTScript is BaseScript, BGTDeployer, ForceTransferBera {
+contract DeployBGTScript is BaseScript, ForceTransferBera {
     uint256 internal constant TESTNET_RESERVE_BERA_AMOUNT = 30e6 ether; // 30M
 
     function run() public broadcast {
-        address bgt = deployBGT(msg.sender, BGT_SALT);
+        BGTDeployer bgtDeployer = new BGTDeployer(msg.sender, BGT_SALT);
+        address bgt = address(bgtDeployer.bgt());
         _checkDeploymentAddress("BGT", bgt, BGT_ADDRESS);
 
         // NOTE: DOUBLE CHECK TESTNET ENV FLAG BEFORE DEPLOYMENT
